a7677de2d08b6dc1ecaecaa6107d8a6677abbb60
lauramosa <lauramosa.dev@gmail.com>
2022-08-11 15:45:35 -0300

    aprendendo objeto

--- main.js
+++ main.js
@@ -22,10 +22,9 @@ console.log(`Meu nome é ${firstName.toUpperCase()} ${lastName.toLowerCase()}`);
 // cortar em listas
 
 const names = "Felipe,João,Júlia";
-const name = "Laura";
 
 console.log(names.split(",")); // com a vírgula
-console.log(name.split("")); // sem a vírgula
+console.log(names.split("")); // sem a vírgula
 
 
 // Operações matemáticas
@@ -74,4 +73,51 @@ const sortedList = list.sort(); // .sort deixará a lista em ordem alfabética
 console.log(sortedList);
 
 console.log(list.length); // .length vai informar quantos itens a lista possui
+
+
+// Exemplo de Objeto
+
+const person = {
+    name: "Laura",
+    age: "23",
+    hobbies: ["Ler", "Jogar", "Correr"],
+};
+
+const {name, age, hobbies} = person;
+
+console.log(name);
+console.log(age);
+console.log(hobbies);
+
+// Outro exemplo
+
+const todos = [
+    {
+        id: 1,
+        description: "Estudar",
+        isCompleted: false,
+    },
+    {
+        id: 2,
+        description: "Ler",
+        isCompleted: true,
+    },
+    {
+        id: 3,
+        description: "Treinar",
+        isCompleted: true,
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
+
 
